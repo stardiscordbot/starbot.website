a63c7e23d6b1f3a09ea40505a62b1e41bc1c7aab
ADG <2007andredoe@gmail.com>
2023-03-11 11:47:19 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,11 @@ const { Client, GatewayIntentBits } = require('discord.js')
 const bot = new Client({
   intents: [
     GatewayIntentBits.Guilds
-  ]
+  ],
+  rest: {
+		api: 'https://discordapp.com/api',
+		version: '9'
+	}
 })
 
 bot.login(process.env.TOKEN)
